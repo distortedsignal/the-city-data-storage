8baec91a78993705cbf842bfaea44ffe2904a163
Tom <distortedsignal@gmail.com>
2014-11-23 18:36:42 -0600

    Get request returns 401, trying to add headers.

--- getAttendees.py
+++ getAttendees.py
@@ -1,4 +1,37 @@
+import ast
+import urllib.parse, urllib.request
+import time
+import hmac
+import hashlib
+import base64
+import json
+
 # Set up Auth for The City
+# Get secret key from file
+conf = open('auth.conf', 'r', encoding='utf-8')
+confStruct = ast.literal_eval(conf.read())
+
+now = int(time.time())
+verb = "GET"
+
+url = 'https://api.onthecity.org/users'
+
+string_to_sign = str(now) + verb + url
+
+unencoded_hmac = urllib.parse.quote(hmac.new(confStruct['secret-key'].encode('utf-8'),
+	string_to_sign.encode('utf-8'), hashlib.sha256).digest())
+
+encoded_hmac = base64.b64encode(unencoded_hmac.encode('utf-8')).strip()
+
+headers = {
+	"X-City-Sig": str(encoded_hmac),
+	"X-City-User-Token": confStruct["user-token"],
+	"Accept": 'application/vnd.thecity.admin.v1+json',
+	"X-City-Time": now
+}
+
+a = urllib.request.urlopen(url, json.dumps(headers)).read()
+
 
 
 
