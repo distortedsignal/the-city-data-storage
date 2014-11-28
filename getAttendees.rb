020882835ef27f37f980987ba6f2c6357514ef30
Tom <distortedsignal@gmail.com>
2014-11-27 22:00:24 -0600

    Print all users in The City database. Minor refactor to make things a little easier to understand.

--- getAttendees.rb
+++ getAttendees.rb
@@ -6,33 +6,70 @@ require 'net/http'
 require 'net/https'
 require 'time'
 
-# This code ripped from https://github.com/thecity/thecity-admin-ruby/blob/0b0d0c019143b7c04bfb583c3e69567321017027/examples/thecity_headers.rb
-unix_time = Time.now.to_i
-http_verb = "GET"
-string_to_sign = "#{unix_time}#{http_verb}https://api.onthecity.org/users"
+def get_user_url(page)
+	url_base = "https://api.onthecity.org"
+	url_path = "/users"
+	url_params = "?page=" + page.to_s
 
-# Get secret key
-file = File.read('auth.conf')
-auth_dict = JSON.parse(file)
+	return "#{url_base}#{url_path}#{url_params}"
+end
 
-secret_key = auth_dict['secret-key']
-uncoded_hmac = OpenSSL::HMAC.digest('sha256', secret_key, string_to_sign)
-unescaped_hmac = Base64.encode64(uncoded_hmac).chomp
-hmac_signature = CGI.escape(unescaped_hmac)
+def get_string_to_sign(unix_time, http_verb, full_url)
+	return "#{unix_time}#{http_verb}#{full_url}"
+end
 
-headers = {}
-headers['X-City-Sig'] = hmac_signature
-headers['X-City-User-Token'] = auth_dict['user-token']
-headers['Accept'] = 'application/vnd.thecity.admin.v1+json'
-headers['X-City-Time'] = unix_time.to_s
-headers['Content-Type'] = 'application/json'
+def sign_string(secret_key, string_to_sign)
+	uncoded_hmac = OpenSSL::HMAC.digest('sha256', secret_key, string_to_sign)
+	unescaped_hmac = Base64.encode64(uncoded_hmac).chomp
+	return CGI.escape(unescaped_hmac)
+end
 
-uri = URI('https://api.onthecity.org/users')
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE #TODO This.
-request = Net::HTTP::Get.new(uri, initheaders = headers)
-response = http.request(request)
+def get_user_page(page)
+	# This code ripped from https://github.com/thecity/thecity-admin-ruby/blob/0b0d0c019143b7c04bfb583c3e69567321017027/examples/thecity_headers.rb
+	full_url = get_user_url(page)
 
-puts response
+	unix_time = Time.now.to_i
+	string_to_sign = get_string_to_sign(unix_time, "GET", full_url)
+
+	# Get secret key
+	file = File.read('auth.conf')
+	auth_dict = JSON.parse(file)
+
+	hmac_signature = sign_string(auth_dict['secret-key'], string_to_sign)
+
+	headers = {}
+	headers['X-City-Sig'] = hmac_signature
+	headers['X-City-User-Token'] = auth_dict['user-token']
+	headers['Accept'] = 'application/vnd.thecity.admin.v1+json'
+	headers['X-City-Time'] = unix_time.to_s
+	headers['Content-Type'] = 'application/json'
+
+	uri = URI("#{full_url}")
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE #TODO This.
+	request = Net::HTTP::Get.new(uri, initheaders = headers)
+	return http.request(request)
+end
+
+response = get_user_page(1)
+response_body = JSON.parse(response.body())
+pages = response_body['total_pages']
+
+# For each page in the database
+(1..pages).each do |page_num|
+	# Get a page by number
+	page = get_user_page(page_num)
+	# Get the page body in a way that we can use it
+	page_body = JSON.parse(page.body())
+	# Get the user list
+	page_body['users'].each do |user|
+		puts user
+	end
+end
+
+
+
+
+
 
